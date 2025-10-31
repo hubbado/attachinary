ebeb7103ff25914a0d2df236ef0ad21a5128fc7a
Alfonso Uceda <alfonso@hubbado.com>
2025-10-31 16:28:30 +0100

    Duplicated initializer attachinary.include_view_helpers is removed

--- lib/attachinary/engine.rb
+++ lib/attachinary/engine.rb
@@ -8,11 +8,7 @@ module Attachinary
     initializer "attachinary.include_view_helpers" do |app|
       ActiveSupport.on_load :action_view do
         include ViewHelpers
-      end
-    end
 
-    initializer "attachinary.include_view_helpers" do |app|
-      ActiveSupport.on_load :action_view do
         ActionView::Helpers::FormBuilder.send(:include, Attachinary::FormBuilder)
       end
     end
@@ -20,6 +16,5 @@ module Attachinary
     initializer "attachinary.enable_simple_form" do |app|
       require "attachinary/simple_form" if defined?(::SimpleForm::Inputs::Base)
     end
-
   end
 end
